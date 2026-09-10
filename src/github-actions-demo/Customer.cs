3ae3f436e39bb362a643007563ff5036824b9812
kuvpatel <kuvpatel@yahoo.co.uk>
2026-09-10 18:01:38 +0100

    initial commit

---
+++ src/github-actions-demo/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github_actions_demo
+{
+    public class customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ src/github-actions-demo/CustomerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github_actions_demo
+{
+    internal class CustomerService
+    {
+    }
+}
