a3578ef48325a1ceb220bd8ccafd77205340543f
kuvpatel <kuvpatel@yahoo.co.uk>
2026-09-10 18:48:10 +0100

    upload build artifact

--- src/github-actions-demo/Customer.cs
+++ src/github-actions-demo/Customer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace github_actions_demo
+﻿namespace github_actions_demo
 {
     public class Customer
     {
--- src/github-actions-demo/CustomerService.cs
+++ src/github-actions-demo/CustomerService.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace github_actions_demo
+﻿namespace github_actions_demo
 {
     public class CustomerService
     {
         public string GetFullName(Customer customer)
         {
             return $"{customer.FirstName} {customer.LastName}";
+        }
+
+        public string GetCountry(Customer customer)
+        {
+            return $"{customer.Country}";
         }
     }
 }
