5772ed39bbc4bf994828eb28e53e49b6b316f581
kuvpatel <kuvpatel@yahoo.co.uk>
2026-09-10 18:20:04 +0100

    add customer country

--- src/github-actions-demo/Customer.cs
+++ src/github-actions-demo/Customer.cs
@@ -12,5 +12,6 @@ namespace github_actions_demo
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
     }
 }
